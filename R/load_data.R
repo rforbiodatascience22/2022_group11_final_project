556393249a128e7333b93527d506a728316f14a5
danielromeroy <s220260@student.dtu.dk>
2022-04-24 01:00:13 +0200

    Data load now generates tidy data

--- R/load_data.R
+++ R/load_data.R
@@ -7,50 +7,77 @@ raw_data = read_tsv("./_raw/GSE13937_series_matrix.txt",
 
 # Set column and row names and transpose the data
 raw_data_flipped = raw_data %>% 
-  mutate(X1 = X1 %>% str_replace("!", ""))
+  mutate(X1 = X1 %>% str_replace("!", "")) %>% 
   # ??? Better way of doing this? I've tried mutate with case_when (unsuccesfully) 
   mutate(X1 = case_when(str_sub(X2, end = 6) == "cohort" ~ "cohort",
-                        str_sub(X2, end = 18) == "hybridization date" ~"hybridization date", # ??? respect max 80 chars
-                        str_sub(X2, end = 11) == "tissue type" ~ "tissue type",
+                        str_sub(X2, end = 18) == "hybridization date" ~"hybridization_date", # ??? respect max 80 chars
+                        str_sub(X2, end = 11) == "tissue type" ~ "tissue_type",
                         str_sub(X2, end = 9) == "histology" ~ "histology",
-                        str_sub(X2, end = 19) == "barrett's esophagus" ~ "barrett's esophagus",
-                        str_sub(X2, end = 22) == "chemoradiation therapy" ~ "chemoradiation therapy",
-                        str_sub(X2, end = 19) == "alcohol consumption" ~ "alcohol consumption",
+                        str_sub(X2, end = 19) == "barrett's esophagus" ~ "barretts_esophagus",
+                        str_sub(X2, end = 22) == "chemoradiation therapy" ~ "chemoradiation_therapy",
+                        str_sub(X2, end = 19) == "alcohol consumption" ~ "alcohol_consumption",
                         str_sub(X2, end = 7) == "smoking" ~ "smoking",
-                        str_sub(X2, end = 10) == "ptnm stage" ~ "ptnm stage",
-                        str_sub(X2, end = 17) == "nodal involvement" ~ "nodal involvement",
-                        str_sub(X2, end = 13) == "survival days" ~ "survival days",
-                        str_sub(X2, end = 19) == "death due to cancer" ~ "death due to cancer",
+                        str_sub(X2, end = 10) == "ptnm stage" ~ "ptnm_stage",
+                        str_sub(X2, end = 17) == "nodal involvement" ~ "nodal_involvement",
+                        str_sub(X2, end = 13) == "survival days" ~ "survival_days",
+                        str_sub(X2, end = 19) == "death due to cancer" ~ "death_due_to_cancer",
                         TRUE ~ X1)) %>%
   column_to_rownames("X1") %>% 
   t() %>% # ??? Transpose, there must be a tidyverse way of doing this
-  as_tibble() %>%
-  column_to_rownames("Sample_title")
+  as_tibble()
   
 write_csv(raw_data_flipped, 
-          file = "./data/data_clean.csv")
+          file = "./data/data_transposed.csv")
 
 
-# TODO: move this part of the wrangling into a new  
-  
-# Remove unnecessary columns, most of these have all the same value
-raw_data_concise = raw_data_flipped %>% 
-  select(-c(Sample_status,
-            Sample_submission_date,
-            Sample_last_update_date,
-            Sample_type,  # Always RNA
-            Sample_channel_count,
+# Remove unnecessary columns, most of these have all the same value or are
+# completely irrelevant
+data_concise = raw_data_flipped %>% 
+  select(-c(Sample_status:Sample_channel_count,
             Sample_organism_ch1,
-            Sample_molecule_ch1,
-            Sample_extract_protocol_ch1,
-            Sample_label_ch1,
-            Sample_label_protocol_ch1,
-            Sample_taxid_ch1,
-            Sample_hyb_protocol,
-            Sample_scan_protocol,
-            Sample_description,
-            Sample_data_processing,
-            Sample_platform_id,
-            Sample_contact_name:Sample_contact_country)) %>% View()  # TODO: Add the rest of the irrelevant columns
+            hybridization_date,
+            histology,
+            Sample_molecule_ch1:Sample_platform_id,
+            Sample_contact_name:Sample_contact_country,
+            Sample_supplementary_file:ID_REF))
+
+
+data_tidy = data_concise %>% 
+  separate(col = Sample_title,
+           into = c(NA, "patient")) %>% 
+  mutate(patient = str_sub(string = patient,
+                           end = -2)) %>% 
+  separate(col = Sample_source_name_ch1,
+           sep = " ",
+           into = c("tumor_type", NA, "tissue_type")) %>% 
+  mutate(cohort = str_sub(string = cohort,
+                          start = 9)) %>%
+  mutate(barretts_esophagus = str_sub(string = barretts_esophagus,
+                              start = 22)) %>%
+  mutate(chemoradiation_therapy = str_sub(string = chemoradiation_therapy,
+                                          start = 25)) %>% 
+  mutate(alcohol_consumption = str_sub(string = alcohol_consumption,
+                                       start = 21)) %>% 
+  mutate(smoking = str_sub(string = smoking,
+                           start = 10)) %>%
+  mutate(ptnm_stage = str_sub(string = ptnm_stage,
+                              start = 13)) %>%
+  mutate(nodal_involvement = str_sub(string = nodal_involvement,
+                                     start = 20)) %>%
+  mutate(survival_days = str_sub(string = survival_days,
+                                 start = 16),
+         survival_days = as.integer(survival_days)) %>% 
+  mutate(death_due_to_cancer = str_sub(string = death_due_to_cancer,
+                                       start = 22)) %>% View()
+
+
+write_csv(data_tidy, 
+          file = "./data/data_tidy.csv")
+
+
+
+
+
+
 
 
