416de4c5bd14d8fbfafb55ce2e3d85d58ba1c93a
danielromeroy <s220260@student.dtu.dk>
2022-05-08 13:19:30 +0200

    Updated README and doit

--- R/00_doit.R
+++ R/00_doit.R
@@ -2,7 +2,11 @@
 source(file = "R/01_load.R")
 source(file = "R/02_clean.R")
 source(file = "R/03_augment.R")
-source(file = "R/04_differential_expression_analysis.R")
-source(file = "R/99_diffexp_plot_composit.R")
+source(file = "R/04_model_kaplan.R")
+source(file = "R/05_model_pca.R")
+source(file = "R/06_model_diffexp.R")
+source(file = "R/07_model_diffexp_plot.R")
 rmarkdown::render(input = "doc/presentation.Rmd",
                   output_file = "presentation.html")
+rmarkdown::render(input = "README.Rmd",
+                  output_file = "README.md")
