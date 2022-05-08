dc6dd8b0c7d9ce97aa29385707a69a35e9f6f5bc
Álvaro León <alvaroguleon@gmail.com>
2022-05-08 14:39:01 +0200

    a

--- R/00_doit.R
+++ R/00_doit.R
@@ -7,6 +7,7 @@ source(file = "R/05_model_pca.R")
 source(file = "R/06_model_diffexp.R")
 source(file = "R/07_model_diffexp_plot.R")
 source(file = "R/08_flowchart.R")
+ource(file = "R/09_kaplan_meier_1.R")
 rmarkdown::render(input = "doc/presentation.Rmd",
                   output_file = "presentation.html")
 rmarkdown::render(input = "README.Rmd",
