f11c072712f107720798f2ab7edc436ab37b539e
jonasdalsberg <s213551@student.dtu.dk>
2022-05-08 08:15:46 +0000

    Updated my code to match the Tidyverse style

--- R/plot_bar_5ysr.R
+++ R/plot_bar_5ysr.R
@@ -11,14 +11,16 @@ data <- read_csv("data/data_aug_wide.csv",
 # Make the data suitable for plotting 5-year survival rates
 data_fysr <- data %>%
   mutate(alive_after_five_years = case_when(survival_days == "1826" ~ 1,
-                                            TRUE ~ 0)) %>% # if alive after 5 years ~ 1, else ~ 0 %>%
-  relocate(alive_after_five_years, .after = survival_days) %>% # move column to the right of survival_days
+                                            TRUE ~ 0)) %>%
+  relocate(alive_after_five_years,
+           .after = survival_days) %>%
   group_by(SEER_stage) %>%
+  # Make sorting by SEER stage possible:
   mutate(SEER_stage = factor(SEER_stage,
                              levels = c("In situ",
                                         "Localized",
                                         "Regional",
-                                        "Distant"))) %>% # makes sorting by stage possible
+                                        "Distant"))) %>%
   filter(tissue_type == "cancerous") # only one row per patient
 
 # Plot: How many patients in each group (cancer stage)
@@ -37,7 +39,9 @@ p_count <- data_fysr %>%
 
 # Plot: 5-year survival rate per group
 p_fysr <- data_fysr %>%
-summarize (alive_sum = sum(alive_after_five_years), counts=n(), five_year_survival_rate = alive_sum / counts) %>%
+summarize (alive_sum = sum(alive_after_five_years),
+           counts=n(),
+           five_year_survival_rate = alive_sum / counts) %>%
   ggplot(mapping = aes(x = SEER_stage,
                        y = five_year_survival_rate,
                        fill = SEER_stage)) +
@@ -54,8 +58,10 @@ summarize (alive_sum = sum(alive_after_five_years), counts=n(), five_year_surviv
 data_fysr <- data_fysr %>%
   mutate(status = case_when(alive_after_five_years == 1 ~ 0,
                                        alive_after_five_years == 0 ~ 1)) %>%
-  relocate(status, .after = alive_after_five_years)
-surv_model <- survfit(Surv(survival_days, status) ~ SEER_stage,
+  relocate(status,
+           .after = alive_after_five_years)
+surv_model <- survfit(Surv(survival_days,
+                           status) ~ SEER_stage,
                  data = data_fysr)
 p_km <- ggsurvplot(surv_model,
                    data = data_fysr,
@@ -65,24 +71,28 @@ p_km <- ggsurvplot(surv_model,
                    font.tickslab = text_size,
                    pval = TRUE)
 
-# Putting the plots together
-p_km <- p_km$plot # extract ggplot object (required for plotting together with ggplots)
+## Putting the plots together
+# Extract ggplot2 object (required for plotting together with ggplots)
+p_km <- p_km$plot
 box_plots <- ggarrange(p_count,
                        p_fysr,
-                       labels = c("A", "B"),
+                       labels = c("A",
+                                  "B"),
                        font.label = c(size = text_size),
                        common.legend = TRUE,
                        legend = "bottom")
 all_plots <- ggarrange(box_plots,
                        p_km,
                        nrow = 2,
-                       heights = c(1,2),
-                       labels = c("", "C"),
+                       heights = c(1,
+                                   2),
+                       labels = c("",
+                                  "C"),
                        font.label = c(size = text_size))
 # Add shared title
+text <- "Survival probability for different stages of cancer\n"
 annotate_figure(all_plots,
-                top = text_grob("Survival probability for different stages of cancer\n",
-                                # face = "bold",
+                top = text_grob(text,
                                 size = 28,
                                 lineheight = 0.3))
 
@@ -95,7 +105,8 @@ ggsave("results/kaplan_meier_seer.png",
 
 
 ### Testing: Japan vs. US
-surv_us_jap <- survfit(Surv(survival_days, status) ~ cohort,
+surv_us_jap <- survfit(Surv(survival_days,
+                            status) ~ cohort,
                        data = data_fysr)
 plot_us_jap <- ggsurvplot(surv_us_jap,
                           data = data_fysr,
@@ -115,7 +126,8 @@ plot_us_jap <- plot_us_jap$plot
 data_us_jap_norm <- data_fysr %>%
   filter(tumor_type == "SCC",
          barretts_esophagus == "No")
-surv_us_jap_norm <- survfit(Surv(survival_days, status) ~ cohort,
+surv_us_jap_norm <- survfit(Surv(survival_days,
+                                 status) ~ cohort,
                           data = data_us_jap_norm)
 plot_us_jap_norm <- ggsurvplot(surv_us_jap_norm,
                              data = data_us_jap_norm,
@@ -128,12 +140,17 @@ plot_us_jap_norm <- plot_us_jap_norm$plot
 us_vs_jap <- ggarrange(plot_us_jap,
                        plot_us_jap_norm,
                        common.legend = TRUE)
+text_1 <- "There are no ADC samples in the Japanese cohort."
+text_2 <- "Thus, the left graph is not a fair comparison."
+text_all <- paste(text_1,
+                  text_2,
+                  sep="\n")
+text_all
 annotate_figure(us_vs_jap,
                 top = text_grob("Survival probability by cohort\n",
                                 size = 28,
                                 lineheight = 0.3),
-                bottom = text_grob("There are no ADC samples in the Japanese cohort.
-                                   Thus, the left graph is not a fair comparison.",
+                bottom = text_grob(text_all,
                                    hjust = 1,
                                    x = 1,
                                    face = "italic"))
@@ -164,16 +181,16 @@ ggsave("results/kaplan_meier_us_jap.png",
 # SEER_stage = 0.021
 # ptnm_stage = 0.025
 # tumor_type = 0.037
-# -------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # nodal_involvement = 0.073
 # chemoradiation_therapy = 0.075
 # alcohol_consumption = 0.38
 # smoking = 0.98
 # 
-# _______________________________________________________________________
+# ______________________________________________________________________________
 # # p-values for different comparisons (ADC only):
 # death_due_to_cancer < 0.0001
-# -------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # ptnm_stage = 0.49
 # SEER_stage = 0.73
 # barretts_esophagus = 0.074
@@ -182,12 +199,12 @@ ggsave("results/kaplan_meier_us_jap.png",
 # alcohol_consumption = 0.53
 # smoking = 0.97
 # 
-# _______________________________________________________________________
+# ______________________________________________________________________________
 # # p-values for different comparisons (SCC only):
 # death_due_to_cancer = 0.0075
 # SEER_stage = 0.016
 # ptnm_stage = 0.027
-# -------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # chemoradiation_therapy = 0.12
 # nodal_involvement = 0.26
 # alcohol_consumption = 0.52
@@ -208,8 +225,9 @@ plot_barr <- ggsurvplot(surv_barr,
                                         "Present"),
                         legend.title = "Barrett's esophagus"
                         )
-plot_barr <- plot_barr$plot + theme(legend.text = element_text(size = text_size),
-                                    legend.title = element_text(size = text_size))
+plot_barr <- plot_barr$plot +
+  theme(legend.text = element_text(size = text_size),
+        legend.title = element_text(size = text_size))
 
 # SEER stage
 surv_seer <- survfit(Surv(survival_days,
@@ -227,8 +245,9 @@ plot_seer <- ggsurvplot(surv_seer,
                         "Distant"),
                         legend.title = "SEER stage"
 )
-plot_seer <- plot_seer$plot + theme(legend.text = element_text(size = text_size),
-                                    legend.title = element_text(size = text_size))
+plot_seer <- plot_seer$plot +
+  theme(legend.text = element_text(size = text_size),
+        legend.title = element_text(size = text_size))
 
 # pTNM stage
 surv_ptnm <- survfit(Surv(survival_days,
@@ -249,8 +268,9 @@ plot_ptnm <- ggsurvplot(surv_ptnm,
                                         "IV"),
                         legend.title = "pTNM stage"
 )
-plot_ptnm <- plot_ptnm$plot + theme(legend.text = element_text(size = text_size),
-                                    legend.title = element_text(size = text_size))
+plot_ptnm <- plot_ptnm$plot +
+  theme(legend.text = element_text(size = text_size),
+        legend.title = element_text(size = text_size))
 
 # Tumor type
 surv_tumo <- survfit(Surv(survival_days,
@@ -261,18 +281,23 @@ plot_tumo <- ggsurvplot(surv_tumo,
                         font.x = text_size,
                         font.y = text_size,
                         font.tickslab = text_size,
-                        pval = TRUE
+                        pval = TRUE,
+                        legend.title = "Tumor type",
+                        legend.labs = c("ADC",
+                                        "SCC")
 )
-plot_tumo <- plot_tumo$plot + theme(legend.text = element_text(size = text_size),
-                                    legend.title = element_text(size = text_size))
+plot_tumo <- plot_tumo$plot +
+  theme(legend.text = element_text(size = text_size),
+        legend.title = element_text(size = text_size))
 
 # Plot all of them together
 all_strata <- ggarrange(plot_tumo,
                         plot_barr,
                         plot_seer,
                         plot_ptnm)
+text <- "Visualization of all significantly different strata\n"
 annotate_figure(all_strata,
-                top = text_grob("Visualization of all significantly different strata\n",
+                top = text_grob(text,
                                 size = 28,
                                 lineheight = 0.3))
 ggsave("results/kaplan_meier_all.png",
@@ -303,8 +328,9 @@ p_tumo <- ggsurvplot(surv_tumo,
                      pval = TRUE,
                      legend = "none"
 )
-p_tumo <- p_tumo$plot + theme(legend.text = element_text(size = text_size),
-                              legend.title = element_text(size = text_size))
+p_tumo <- p_tumo$plot +
+  theme(legend.text = element_text(size = text_size),
+        legend.title = element_text(size = text_size))
 
 # Combined tumor type plot
 tumo_plots <- ggarrange(p_count_tumo,
